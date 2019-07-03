ed67c3eb3e985f5f1592d5253d2f6239423ebcac
Matthew Miller <matthew@mi11er.net>
2019-07-03 12:15:13 -0400

    feat: 🎸 add mo formula

---
+++ Formula/mo-bash.rb
@@ -0,0 +1,15 @@
+class MoBash < Formula
+    url "https://github.com/tests-always-included/mo/archive/2.0.4.tar.gz"
+    version "2.0.4"
+    sha256 "1aafe99edd96da1a30dd4fae0154de8be03995879c80852f733e7fb1bb04c676"
+
+    def install
+      bin.install "mo"
+    end
+  end
+
+
+
+
+
+
