4984a8621a4fc36764aa55829ae8065042dbde90
Matthew Miller <matthew@mi11er.net>
2020-09-30 20:19:31 -0400

    feature(app): update wrangler

--- Formula/cloudflare-wrangler.rb
+++ Formula/cloudflare-wrangler.rb
@@ -3,9 +3,9 @@ require "language/node"
 class CloudflareWrangler < Formula
   desc "Wrangle your Cloudflare Workers"
   homepage "https://github.com/cloudflare/wrangler#readme"
-  url "https://registry.npmjs.org/@cloudflare/wrangler/-/wrangler-1.2.0.tgz"
-  version "1.2.0"
-  sha256 "2162e42d1fac6e3c252378a358016c9a3085d1769ded4327104995e0bdbc13a9"
+  url "https://registry.npmjs.org/@cloudflare/wrangler/-/wrangler-1.11.0.tgz"
+  version "1.11.0"
+  sha256 "2e80f5c212edf69f8fc267c4d18fabe7f84e03f12a7a9cf8e0123e260f024846"
 
   depends_on "node"
   depends_on "python" => :build
